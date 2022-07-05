b905024d66b51a1863377338965393b925d7bf4c
kirill3518 <kirill3518@mail.ru>
2022-07-05 13:13:43 +0500

    Project create

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import { Book } from './book.js'
+import { books } from './book-collection.js'
+
+function findSuitableBook (genre: string, pagesLimit: number, multipleRecommendations = true): Book | Book[] {
+  const findAlgorithm = (book: Book) => {
+    return book.genre === genre && book.pagesAmount <= pagesLimit
+  }
+
+  if (multipleRecommendations) {
+    return books.filter(findAlgorithm)
+  } else {
+    return books.find(findAlgorithm)
+  }
+}
+
+const recommendedBook = findSuitableBook('fantasy', 1000)
+
+if (recommendedBook instanceof Book) {
+  console.log(recommendedBook.name)
+} else {
+  console.log(recommendedBook[0].name)
+}
